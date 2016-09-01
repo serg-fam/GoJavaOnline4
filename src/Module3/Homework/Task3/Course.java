4908fb420c377884f1632e084b818c752ffbab36
serg-fam <sergifam@gmail.com>
2016-09-01 17:00:52 +0300

    Module3HomeworkTask3

---
+++ src/Module3/Homework/Task3/Course.java
@@ -0,0 +1,73 @@
+package Module3.Homework.Task3;
+
+import java.util.Date;
+
+/**
+ * Created by Serg-fam on 01.09.2016 on 12:11.
+ */
+class Course {
+
+    private Date startDate;
+    private String name;
+    private int hoursDuration;
+    private String teacherName;
+
+
+
+    Course(Date startDate, String name) {
+
+        this.startDate = startDate;
+        this.name = name;
+    }
+
+    Course(String name, int hoursDuration, String teacherName) {
+
+        this.name = name;
+        this.hoursDuration = hoursDuration;
+        this.teacherName = teacherName;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHoursDuration() {
+        return hoursDuration;
+    }
+
+    public void setHoursDuration(int hoursDuration) {
+        this.hoursDuration = hoursDuration;
+    }
+
+    public String getTeacherName() {
+        return teacherName;
+    }
+
+    public void setTeacherName(String teacherName) {
+        this.teacherName = teacherName;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "startDate=" + startDate +
+                ", name='" + name + '\'' +
+                ", hoursDuration=" + hoursDuration +
+                ", teacherName='" + teacherName + '\'' +
+                '}';
+    }
+
+}
+
---
+++ src/Module3/Homework/Task3/Student.java
@@ -0,0 +1,83 @@
+package Module3.Homework.Task3;
+
+import java.util.Arrays;
+
+/**
+ * Created by Serg-fam on 01.09.2016 on 12:11.
+ */
+class Student {
+
+    private String firstName;
+
+
+    private String lastName;
+    private int group;
+    private Course[] coursesTaken;
+    private int age;
+
+    Student(String firstName, String lastName, int group) {
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.group = group;
+
+    }
+
+    Student(String lastName, Course[] coursesTaken) {
+
+        this.lastName = lastName;
+        this.coursesTaken = coursesTaken;
+    }
+
+    String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    int getGroup() {
+        return group;
+    }
+
+    public void setGroup(int group) {
+        this.group = group;
+    }
+
+    Course[] getCoursesTaken() {
+        return coursesTaken;
+    }
+
+    public void setCoursesTaken(Course[] coursesTaken) {
+        this.coursesTaken = coursesTaken;
+    }
+
+    int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", group=" + group +
+                ", coursesTaken=" + Arrays.toString(coursesTaken) +
+                ", age=" + age +
+                '}';
+    }
+
+}
