ef559bf218266dc09db0252213f5ebbf1f4d1462
Oleg Orlov <info@olegorlov.com>
2016-09-07 18:59:51 +0300

    Module3_1 from mentor

--- src/Module3/Homework/Task1/Bird.java
+++ src/Module3/Homework/Task1/Bird.java
@@ -3,26 +3,18 @@ package Module3.Homework.Task1;
 /**
  * Created by Serg-fam on 01.09.2016 on 10:58.
  */
-class Bird {
-
-    void bird() {
-
-        System.out.println("I am Bird");
-
-    }
-
-    void walk() {
-
-        System.out.println("I am walking");
-    }
+class Bird extends Animal{
 
     void fly() {
-
         System.out.println("I am flying");
     }
-    void sing() {
 
+    void sing() {
         System.out.println("I am singing");
+    }
+
+    void bird() {
+        System.out.println("I am Bird");
     }
 
 }
--- src/Module3/Homework/Task1/Solution.java
+++ src/Module3/Homework/Task1/Solution.java
@@ -7,10 +7,11 @@ public class Solution {
     public static void main(String[] args) {
 
         Bird bird = new Bird();
-
         bird.walk();
         bird.fly();
         bird.sing();
         bird.bird();
+
     }
+
 }
