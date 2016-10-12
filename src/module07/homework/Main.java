b4a72c7c9b6b2f4882b9ee8166764bcb37001227
serg-fam <sergifam@gmail.com>
2016-10-12 17:37:29 +0300

    Module7Homework((2 final)

--- src/module07/homework/Main.java
+++ src/module07/homework/Main.java
@@ -13,9 +13,9 @@ public class Main {
         List<Order> order = new LinkedList<>();
 
         order.add(new Order(10L, 1200, Currency.UAH, "ItemName1", "Identificator1", new User(10L, "FirstName1",
-                "LastName1", "City Kiev", 100)));
+                "LastName1", "City1 Kiev", 100)));
         order.add(new Order(11L, 2200, Currency.USD, "ItemName2", "Identificator2", new User(20L, "FirstName2",
-                "LastName2", "City Kiev", 200)));
+                "LastName2", "City2 Kiev", 200)));
         order.add(new Order(12L, 2300, Currency.UAH, "ItemName3", "Identificator3", new User(30L, "FirstName3",
                 "LastName3", "City3", 300)));
         order.add(new Order(13L, 1400, Currency.USD, "ItemName4", "Identificator4", new User(40L, "FirstName4",
@@ -53,7 +53,8 @@ public class Main {
                 return o1.getUser().getCity().compareTo(o2.getUser().getCity());
             }
         });
-        System.out.println("sort list by Order price in increase order AND User city : " + "\n" + order);
+        System.out.println("sort list by Order price in increase order AND User city :=================================" +
+                "" + "\n" + order);
 
         order.sort(new Comparator<Order>() {
             @Override
@@ -67,58 +68,49 @@ public class Main {
                 return o1.getUser().getCity().compareTo(o2.getUser().getCity());
             }
         });
-        System.out.println("sort list by Order itemName AND ShopIdentificator AND User city : " + "\n" + order);
-
+        System.out.println("sort list by Order itemName AND ShopIdentificator AND User city :==========================" +
+                " " + "\n" + order);
         order.add(new Order(19L, 1000, Currency.USD, "ItemName10", "Identificator10", new User(11L, "FirstName10",
                 "LastName10", "City Kiev", 1000)));
-        Set orderHasSet = new HashSet();
+        Set<Order> orderHasSet = new HashSet<>();
         orderHasSet.addAll(order);
-        List list = new LinkedList();
+        List<Order> list = new LinkedList<>();
         list.addAll(orderHasSet);
-        TreeSet orderTreeSet = new TreeSet();
+        TreeSet<Order> orderTreeSet = new TreeSet<>();
         list.addAll(orderTreeSet);
         order.clear();
         order.addAll(list);
-        System.out.println("List elements without duplicates: " + "\n" + order);
+        System.out.println("List elements without duplicates :======================================================" +
+                " " + "\n" + order);
         order.removeIf(x -> (x.getPrice() < 1500));
-        System.out.println("delete items where price less than 1500 : " + "\n" + order);
+        System.out.println("delete items where price less than 1500 :===============================================" +
+                "\n" + order);
         order.clear();
         order.addAll(list);
 
         order.replaceAll(new UnaryOperator<Order>() {
             @Override
             public Order apply(Order order) {
-                if (order.getCurrency() != Currency.USD) return null;
-                return order;
+                if (order.getCurrency() == Currency.USD) return order;
+                return null;
             }
         });
         System.out.println("separate list orders in USD" + "\n" + order);
-
         order.clear();
         order.addAll(orderHasSet);
-
         order.replaceAll(new UnaryOperator<Order>() {
             @Override
             public Order apply(Order order) {
-                if (order.getCurrency() != Currency.UAH) return null;
-                return order;
+                if (order.getCurrency() == Currency.UAH) return order;
+                return null;
             }
         });
         System.out.println("separate list orders in UAH" + "\n" + order);
-
-        order.clear();
-        order.addAll(orderHasSet);
-        System.out.println("=========================" + "\n" + order);
-
-        order.replaceAll(new UnaryOperator<Order>() {
-            @Override
-            public Order apply(Order order) {
-
-                              return null;
-            }
-        });
-
-        System.out.println("separate list for as many lists as many unique cities are in User : " + "\n" + order);
+        Map<String, Order> map = new HashMap<>();
+        for (Order item : orderHasSet) {
+            map.put(item.getUser().getCity(), item);
+        }
+        System.out.println("separate list for as many lists as many unique cities are in User : "
+                + map.size() + " " + orderHasSet.size() + "\n" + map);
     }
-
 }
