ed47e670dd95e467a1d80d9e5cd1861597af03c0
serg-fam <sergifam@gmail.com>
2016-10-21 11:02:06 +0300

    Module09Homework(Skeleton)

---
+++ src/module09/homewrok/Main.java
@@ -0,0 +1,12 @@
+package module09.homewrok;
+
+import static module07.homework.*;
+
+/**
+ * Created by Serg-fam on 21.10.2016 on 11:00.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
