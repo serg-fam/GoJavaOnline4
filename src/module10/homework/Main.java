91fed33a07e31f29a6693a9ccf0ae885abad86e3
serg-fam <sergifam@gmail.com>
2016-10-28 13:46:08 +0300

    Module10homework(Skeleton)

---
+++ src/module10/homework/Main.java
@@ -0,0 +1,10 @@
+package module10.homework;
+
+/**
+ * Created by Serg-fam on 28.10.2016 on 13:39.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/module10/homework/ThreeException.java
@@ -0,0 +1,7 @@
+package module10.homework;
+
+/**
+ * Created by Serg-fam on 28.10.2016 on 13:45.
+ */
+public class ThreeException {
+}
